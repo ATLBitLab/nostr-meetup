60f887a0f356fa3733af09d7902cbf18db9f2e29
Stephen DeLorme <stephen@d.elor.me>
2023-03-20 14:34:53 -0600

    Landing image

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
 import LandingLayout from '@/components/LandingLayout'
 import Tag from '@/components/Tag'
+import Image from 'next/image'
+import collegeStudents from '../public/college-students.jpg'
 
 export default function Home() {
   const topics = [
@@ -40,8 +42,8 @@ export default function Home() {
   return (
     <>
       <LandingLayout description="Meetup with folks using Nostr meetup">
-        <div className="bg-purple-600 min-h-full p-4 lg:p-16">
-          <div className="bg-white p-4 rounded-md drop-shadow-sm max-w-xl h-auto ml-auto lg:p-8">
+        <div className="bg-purple-600 min-h-full p-4 lg:p-16 lg:pt-24">
+          <div className="relative z-10 bg-white p-4 rounded-md drop-shadow-sm max-w-2xl h-auto ml-auto lg:p-12">
             <h1 className="sr-only">Nostr Meetup</h1>
             <p className="font-display text-3xl mb-4">Find people with shared interests and meetups to attend</p>
             <h2 className="font-display font-bold text-2xl mb-4">Topics</h2>
@@ -51,6 +53,10 @@ export default function Home() {
               ))}
             </div>
           </div>
+          <div className="w-full h-full absolute top-0 left-0">
+            <Image src={collegeStudents} alt="" className="w-full h-full object-cover" />
+          </div>
+          
         </div>
       </LandingLayout>
     </>
