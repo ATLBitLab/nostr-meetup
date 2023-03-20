28e35905d8b8aa66c217e5cd97981be053d75bb2
Stephen DeLorme <stephen@d.elor.me>
2023-03-20 14:41:25 -0600

    Signup WIP

--- pages/login.tsx
+++ pages/login.tsx
@@ -7,7 +7,7 @@ export default function Login() {
   return (
     <>
       <LandingLayout title="Login" description="Log in to Nostr Meetup easily with your Nostr profile.">
-      <div className="bg-purple-600 min-h-full p-4 lg:p-16 lg:pt-24">
+        <div className="bg-purple-600 min-h-full p-4 lg:p-16 lg:pt-24">
           <div className="relative z-10 bg-white p-4 rounded-md drop-shadow-sm max-w-2xl h-auto ml-auto lg:p-12">
             <h1 className="font-display text-3xl">Log in</h1>
             <p className="text-2xl mb-4">Log in with a Nostr browser extension.</p>
--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,11 +1,24 @@
 import Head from 'next/head'
 import LandingLayout from '@/components/LandingLayout'
+import Image from 'next/image'
+import camping from '../public/camping.jpg'
 
 export default function SignUp() {
   return (
     <>
       <LandingLayout title="Sign Up" description="Sign up for Nostr to use Nostr Meetup">
-        <div>This is the sign up page</div>
+      <div className="bg-purple-600 min-h-full p-4 lg:p-16 lg:pt-24">
+          <div className="relative z-10 bg-white p-4 rounded-md drop-shadow-sm max-w-2xl h-auto ml-auto lg:p-12">
+            <h1 className="font-display text-3xl">Sign in</h1>
+            <p className="text-2xl mb-4">
+              Nostr is a censorship resistant social protcol. Catherd uses Nostr to help you find meetups, so you need a Nostr account to sign in with us. If you need help getting setup with a Nostr account, see these resources:
+            </p>
+          </div>
+          <div className="w-full h-full absolute top-0 left-0">
+            <Image src={camping} alt="" className="w-full h-full object-cover" />
+          </div>
+          
+        </div>
       </LandingLayout>
     </>
   )
