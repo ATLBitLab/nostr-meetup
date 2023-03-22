56f92a3b99b75a549c65f3925b06931b6b63e8e5
Stephen DeLorme <stephen@d.elor.me>
2023-03-22 11:02:16 -0600

    Cleanup

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -7,7 +7,7 @@ export default function SignUp() {
   return (
     <>
       <LandingLayout title="Sign Up" description="Sign up for Nostr to use Nostr Meetup">
-      <div className="bg-purple-600 min-h-full p-4 lg:p-16 lg:pt-24">
+        <div className="bg-purple-600 min-h-full p-4 lg:p-16 lg:pt-24">
           <div className="relative z-10 bg-white p-4 rounded-md drop-shadow-sm max-w-2xl h-auto ml-auto lg:p-12">
             <h1 className="font-display text-3xl">Sign in</h1>
             <p className="text-2xl mb-4">
